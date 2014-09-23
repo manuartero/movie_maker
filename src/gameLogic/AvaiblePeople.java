db4ebdabd73423920a211ad82c31fe260a8a3d52
manutero <manutero@gmail.com>
2014-09-23 21:36:13 +0200

    Minnor refactor in AvaiblePeople: using Constant.CONSTANT_NAME

--- src/gameLogic/AvaiblePeople.java
+++ src/gameLogic/AvaiblePeople.java
@@ -5,13 +5,10 @@ import gameLogic.staff.ScriptWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import movieMaker.MovieMaker.Constants;
 import utils.FileHelper;
 import utils.RandomHelper;
 
-/**
- *
- * @author manutero
- */
 public class AvaiblePeople {
 
   private static List<Director> directorList = null;
@@ -20,8 +17,9 @@ public class AvaiblePeople {
   public static List<Director> directorList() {
     if (directorList == null) {
       directorList = new ArrayList<>();
-      List<String> allNames = FileHelper.readTextFile(movieMaker.MovieMaker.DIRECTOR_LIST_FILE_PATH);
-      List<String> selectedNames = RandomHelper.selectQItemsFromList(allNames, 100);
+      List<String> allNames = FileHelper.readTextFile(Constants.DIRECTOR_LIST_FILE_PATH);
+      List<String> selectedNames = RandomHelper
+              .selectQItemsFromList(allNames, Constants.NUMBER_OF_DIRECTORS);
       for (String name : selectedNames) {
         Director director = Director.createNewDirector(name);
         directorList.add(director);
@@ -33,8 +31,9 @@ public class AvaiblePeople {
   public static List<ScriptWriter> scriptWriterList() {
     if (scriptWriterList == null) {
       scriptWriterList = new LinkedList<>();
-      List<String> allNames = FileHelper.readTextFile(movieMaker.MovieMaker.WRITER_LIST_FILE_PATH);
-      List<String> selectedNames = RandomHelper.selectQItemsFromList(allNames, 100);
+      List<String> allNames = FileHelper.readTextFile(Constants.WRITER_LIST_FILE_PATH);
+      List<String> selectedNames = RandomHelper
+              .selectQItemsFromList(allNames, Constants.NUMBER_OF_WRITERS);
       for (String name : selectedNames) {
         ScriptWriter writer = ScriptWriter.createNewScriptWriter(name);
         scriptWriterList.add(writer);
