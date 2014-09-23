909c4c0f1b8b63cd217c3a43792ba26fdb734ea8
manutero <manutero@gmail.com>
2014-09-23 21:42:24 +0200

    Minnor refactor: added MovieMaker.Constants to MovieMaker.java

--- src/movieMaker/MovieMaker.java
+++ src/movieMaker/MovieMaker.java
@@ -2,6 +2,8 @@ package movieMaker;
 
 import controller.Controller;
 import gui.MainFrame;
+import java.awt.Color;
+import java.awt.Font;
 import javax.swing.SwingUtilities;
 
 /**
@@ -12,22 +14,29 @@ import javax.swing.SwingUtilities;
  */
 public class MovieMaker {
 
-  /* Project constants */
-  public static String DIRECTOR_LIST_FILE_PATH
-          = MovieMaker.class.getClassLoader().getResource("resources/directors.txt").getPath();
+  public static class Constants {
 
-  public static String WRITER_LIST_FILE_PATH
-          = MovieMaker.class.getClassLoader().getResource("resources/writers.txt").getPath();
+    public static String DIRECTOR_LIST_FILE_PATH
+            = MovieMaker.class.getClassLoader().getResource("resources/directors.txt").getPath();
 
-  public static String ACTOR_LIST_FILE_PATH
-          = MovieMaker.class.getClassLoader().getResource("resources/actors.txt").getPath();
+    public static String WRITER_LIST_FILE_PATH
+            = MovieMaker.class.getClassLoader().getResource("resources/writers.txt").getPath();
 
-  public static String PORTRAITS_FOLDER = "portraits/";
+    public static String ACTOR_LIST_FILE_PATH
+            = MovieMaker.class.getClassLoader().getResource("resources/actors.txt").getPath();
 
-  public static String THUMBNAILS_FOLDER = "thumbnails/";
+    public static final String PORTRAITS_FOLDER = "portraits/";
+    public static final String THUMBNAILS_FOLDER = "thumbnails/";
+    public static final String ICONS_FOLDER = "icons/";
 
-  public static String ICONS_FOLDER = "icons/";
+    public static final String NO_TEXT = "-";
+    public static final Color FONT_COLOR = new Color(54, 54, 54);
+    public static final Color SPECIAL_COLOR = new Color(32, 182, 182);
+    public static final Font FONT = new Font("Menlo", 0, 13);
 
+    public static final int NUMBER_OF_DIRECTORS = 100;
+    public static final int NUMBER_OF_WRITERS = 100;
+  }
 
   /**
    * Lunch the app.
